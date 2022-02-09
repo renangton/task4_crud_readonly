7ca5347029302055fb81b5d3cb0645461260ae10
renangton <puccho0427@gmail.com>
2022-02-09 23:04:59 +0900

    不要なコメントと改行の削除、フォーマッターの導入 #issue6

--- src/main/java/com/crud_readonly/service/GameService.java
+++ src/main/java/com/crud_readonly/service/GameService.java
@@ -8,28 +8,24 @@ import org.springframework.stereotype.Service;
 import com.crud_readonly.entity.Game;
 import com.crud_readonly.mapper.GameMapper;
 
-// SQLの実行結果を取得する
 @Service
 public class GameService {
-	
+
 	@Autowired
 	GameMapper gameMapper;
 
-	// 全件取得(昇順)
-	public List<Game> getGamesAsc(){
-		
+	public List<Game> getGamesAsc() {
+
 		return gameMapper.findAllAsc();
 
 	}
-	
-	// 全件取得(降順)
-	public List<Game> getGamesDesc(){
-		
+
+	public List<Game> getGamesDesc() {
+
 		return gameMapper.findAllDesc();
-		
+
 	}
-	
-    // idで検索
+
 	public Game findById(String id) {
 		Game gameId = new Game();
 		gameId.setId(id);
