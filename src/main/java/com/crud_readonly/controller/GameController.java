1acdd68de8963aaaae0939cf28fbb9f61b02cbdc
renangton <puccho0427@gmail.com>
2022-02-09 23:25:36 +0900

    Apache Commons Lang 3のStringUtilsを使用して空文字比較 #7

--- src/main/java/com/crud_readonly/controller/GameController.java
+++ src/main/java/com/crud_readonly/controller/GameController.java
@@ -1,5 +1,9 @@
 package com.crud_readonly.controller;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,71 +11,67 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
-import org.apache.commons.lang3.math.NumberUtils;
-
 import com.crud_readonly.entity.Game;
 import com.crud_readonly.form.GameForm;
 import com.crud_readonly.service.GameService;
 
 @Controller
 public class GameController {
-	
+
 	@ModelAttribute
 	GameForm setupForm() {
 		return new GameForm();
 	}
-	
+
 	@Autowired
 	GameService gameService;
-	
+
 	@GetMapping("/search")
 	public String getSearch() {
-		
+
 		// serarch.htmlに画面遷移
 		return "search";
-		
+
 	}
-	
+
 	// [/games]へのHTTPリクエストを受け付ける
 	@PostMapping("/search/db")
-	
+
 	public String search(GameForm gameForm, Model model) {
-		
+
 		// getId()が" "の時、全件取得
-		if(gameForm.getId() == "") {
-			
-			if(gameForm.getOrder().equals("asc")) {
-				
-				List<Game> gameListAsc = gameService.getGamesAsc(); 
+		if (StringUtils.isEmpty(gameForm.getId())) {
+
+			if (gameForm.getOrder().equals("asc")) {
+
+				List<Game> gameListAsc = gameService.getGamesAsc();
 				model.addAttribute("gameList", gameListAsc);
-				
-			}else if(gameForm.getOrder().equals("desc")) {
-				
-				List<Game> gameListDesc = gameService.getGamesDesc(); 
+
+			} else if (gameForm.getOrder().equals("desc")) {
+
+				List<Game> gameListDesc = gameService.getGamesDesc();
 				model.addAttribute("gameList", gameListDesc);
-			}	
-		
-		// getId()が" "でない時、IDで検索し、１件取得
-		}else {	
-			
-			if(NumberUtils.isNumber(gameForm.getId())) {
+			}
+
+			// getId()が" "でない時、IDで検索し、１件取得
+		} else {
+
+			if (NumberUtils.isNumber(gameForm.getId())) {
 				Game game = gameService.findById(gameForm.getId());
 				model.addAttribute("game", game);
-					
-					// レコードが存在しない時の表示
-					if(game == null) {
-						model.addAttribute("notFound", "レコードは存在しませんでした。");
-					}
-			
-			// 文字列で検索された時の表示
-			}else {
+
+				// レコードが存在しない時の表示
+				if (game == null) {
+					model.addAttribute("notFound", "レコードは存在しませんでした。");
+				}
+
+				// 文字列で検索された時の表示
+			} else {
 				model.addAttribute("mojiretsu", "数字を入力して下さい。");
 			}
-		
+
 		}
-		
+
 		return "search/db";
 	}
 }
